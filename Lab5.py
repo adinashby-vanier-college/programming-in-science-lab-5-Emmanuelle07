3080997e535286d4e0fd9105e5103768e8297348
Emmanuelle07 <ebbasseha@outlook.com>
2025-02-28 14:10:24 +0000

    q.2

--- Lab5.py
+++ Lab5.py
@@ -12,7 +12,13 @@ def hollow_square(n):
 # 123
 # 1234
 def number_pattern(n):
-    return ""
+    result = ""
+    for number in range (1, n + 1):
+        for j in range (1, number + 1):
+            result += str(j)
+        result += "\n"
+    return (result.strip())
+
 
 # Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
 def sum_of_natural_numbers(n):
